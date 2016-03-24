2dfb802aeb583c36055232153936deea070abc04
Rex Xia <rexpie@gmail.com>
2016-03-24 20:42:11 +0800

    added bootstrap switch for toggle data read; added chart y axis range options

--- drawCanvas.js
+++ drawCanvas.js
@@ -1,146 +1,190 @@
+var serverURL = 'http://127.0.0.1:23456/';
 
-      var serverURL = 'http://127.0.0.1:23456/';
+// dynamic data points
+var dataPoints = [];
 
-      // dynamic data points
-      var dataPoints = [];
+// chart object
+var chart;
 
-      // chart object
-      var chart;
+var intervalObj;
 
-      var intervalObj;
+// serial read frequency
+var samplingSpeed = 100;
 
-      // serial read frequency
-      var samplingSpeed = 100;
+// max data points length
+var maxLength = 100;
 
-      // max data points length
-      var maxLength = 100;
+var globalX = 0;
 
-      var globalX = 0;
-
-      var needInit = true;
-      var initFinished = false;
+var needInit = true;
+var initFinished = false;
 
 
-      var updating = true;
+var updating = true;
 
-      function initCanvas(lines) {
-        needInit = false;
+function initCanvas(lines) {
+  needInit = false;
 
-        lines.forEach(function(data) {
-          var array = data.split(',');
-          var counter = 0;
-          var columns = array.forEach(function(elem) {
-            dataPoints[counter] = dataPoints[counter] || {
-              type: "line",
-              dataPoints: []
-            };
-            dataPoints[counter].dataPoints.push({x: globalX, y: parseFloat(elem)});
-            counter++;
+  lines.forEach(function(data) {
+    var array = data.split(',');
+    var counter = 0;
+    var columns = array.forEach(function(elem) {
+      dataPoints[counter] = dataPoints[counter] || {
+        type: "line",
+        dataPoints: []
+      };
+      dataPoints[counter].dataPoints.push({
+        x: globalX,
+        y: parseFloat(elem)
+      });
+      counter++;
+    });
+    globalX++;
+  });
+
+  chart = new CanvasJS.Chart("chart", {
+    title: {
+      text: "Serial Data"
+    },
+    axisX: {
+      title: "Counter"
+    },
+    axisY: {
+      title: "Value"
+    },
+    // data: [   {     type: "line",     dataPoints: dataPoints   } ]
+    data: dataPoints
+  });
+
+  initFinished = true;
+}
+
+function startRead() {
+  intervalObj = setInterval(function() {
+
+    $.get(serverURL + "read", function(lines) {
+
+      if (needInit) {
+        initCanvas(lines);
+      }
+
+      if (!initFinished) {
+        return;
+      }
+
+      lines.forEach(function(data) {
+        if (!data || data.length === 0 || !updating) {
+          return;
+        }
+        var array = data.split(',');
+        var counter = 0;
+        var columns = array.forEach(function(elem) {
+          dataPoints[counter] = dataPoints[counter] || {
+            type: "line",
+            dataPoints: []
+          };
+          dataPoints[counter].dataPoints.push({
+            x: globalX,
+            y: parseFloat(elem)
           });
-          globalX++;
-        });
 
-        chart = new CanvasJS.Chart("chart", {
-          title: {
-            text: "Serial Data"
-          },
-          axisX: {
-            title: "Counter"
-          },
-          axisY: {
-            title: "Value"
-          },
-          // data: [   {     type: "line",     dataPoints: dataPoints   } ]
-          data: dataPoints
-        });
-
-        initFinished = true;
-      }
-
-      function startRead() {
-        intervalObj = setInterval(function() {
-
-          $.get(serverURL + "read", function(lines) {
-
-            if (needInit) {
-              initCanvas(lines);
-            }
-
-            if (!initFinished) {
-              return;
-            }
-
-            lines.forEach(function(data) {
-              if (!data || data.length === 0 || !updating){
-                return;
-              }
-              var array = data.split(',');
-              var counter = 0;
-              var columns = array.forEach(function(elem) {
-                dataPoints[counter] = dataPoints[counter] || {
-                  type: "line",
-                  dataPoints: []
-                };
-                dataPoints[counter].dataPoints.push({x: globalX, y: parseFloat(elem)});
-
-                // shift if length too long
-                var length = dataPoints[counter].dataPoints.length;
-                if (length > maxLength){
-                  dataPoints[counter].dataPoints = dataPoints[counter].dataPoints.splice(length - maxLength, maxLength);
-                }
-
-                counter++;
-              });
-              globalX++;
-            });
-            // console.log(dataPoints);
-            chart.render();
-          });
-        }, samplingSpeed);
-      }
-
-      function stopRead() {
-        clearInterval(intervalObj);
-      }
-
-      function _connect(port, baudrate) {
-        $.get(serverURL + "connect?port=" + port + "&baudrate=" + baudrate);
-      }
-
-      function listPorts(listElementId) {
-        $.get(serverURL + "listPorts", function(data) {
-          if (listElementId.indexOf('#') !== 0) {
-            listElementId = "#" + listElementId;
+          // shift if length too long
+          var length = dataPoints[counter].dataPoints.length;
+          if (length > maxLength) {
+            dataPoints[counter].dataPoints = dataPoints[counter].dataPoints.splice(length -
+              maxLength, maxLength);
           }
-          $(listElementId).html("");
-          data.forEach(function(port) {
-            $(listElementId).append('<button type="button" class="btn btn-default" onclick="connect($(this))">' + port.comName + "</button>");
-          });
+
+          counter++;
         });
-      }
+        globalX++;
+      });
+      // console.log(dataPoints);
+      chart.render();
+    });
+  }, samplingSpeed);
+}
 
-      function connect(button){
-        var baudrate = $('#baudrate').val() || $('#baudrate').attr("placeholder");
-        _connect(button.html(), baudrate);
-        startRead();
-      }
+function stopRead() {
+  clearInterval(intervalObj);
+}
 
-      function toggleUpdate(){
-        updating = !updating;
-      }
+function _connect(port, baudrate) {
+  $.get(serverURL + "connect?port=" + port + "&baudrate=" + baudrate);
+}
 
-      function changeMaxLength(length){
-        maxLength = length;
-      }
+function listPorts(listElementId) {
+  $.get(serverURL + "listPorts", function(data) {
+    if (listElementId.indexOf('#') !== 0) {
+      listElementId = "#" + listElementId;
+    }
+    $(listElementId).html("");
+    data.forEach(function(port) {
+      $(listElementId).append(
+        '<button type="button" class="btn btn-default" onclick="connect($(this))">' + port.comName +
+        "</button>");
+    });
+  });
+}
 
-      function writeToSerial(data){
-        $.get(serverURL + "write/" + encodeURI(data));
-      }
+function connect(button) {
+  var baudrate = $('#baudrate').val() || 57600;
+  _connect(button.html(), baudrate);
+  startRead();
+}
 
-      // default action here
-      startRead();
+function toggleUpdate() {
+  updating = !updating;
+}
 
-      setInterval(function() {
-        listPorts("#ports");
-      }, 5000);
-      listPorts("#ports");
+function setUpdate(state) {
+  updating = state;
+}
+
+function changeMaxLength(length) {
+  maxLength = length;
+}
+
+function writeToSerial(data) {
+  $.get(serverURL + "write/" + encodeURI(data));
+}
+
+function setYRange(min, max) {
+  chart.options.axisY.viewportMinimum = min;
+  chart.options.axisY.viewportMaximum = max;
+}
+
+function resetYRange() {
+  delete chart.options.axisY.viewportMinimum;
+  delete chart.options.axisY.viewportMaximum;
+}
+
+// default action here
+startRead();
+
+setInterval(function() {
+  listPorts("#ports");
+}, 5000);
+listPorts("#ports");
+
+$("[type='checkbox']").bootstrapSwitch();
+$("[name='dataFlowControl']").on('switchChange.bootstrapSwitch', function(event, state) {
+  setUpdate(state);
+});
+
+$("[name='chartControl']").on('switchChange.bootstrapSwitch', function(event, state) {
+  if (state) {
+    $('#minmax').show();
+    changeChartRange();
+  } else {
+    $('#minmax').hide();
+    resetYRange();
+  }
+});
+
+
+function changeChartRange() {
+  if ($("#minY").val() !== "" && $("#maxY").val() !== "") {
+    setYRange($("#minY").val(), $("#maxY").val());
+  }
+}
